1f85b098ca0e1f52471dd28170cb1ef5bd303e6e
semka420 <78977783+semka420@users.noreply.github.com>
2026-04-20 23:45:14 +0300

    Update Program.cs

--- Life/Program.cs
+++ Life/Program.cs
@@ -1,8 +1,7 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Threading;
 
 namespace cli_life
@@ -12,6 +11,7 @@ namespace cli_life
         public bool IsAlive;
         public readonly List<Cell> neighbors = new List<Cell>();
         private bool IsAliveNext;
+
         public void DetermineNextLiveState()
         {
             int liveNeighbors = neighbors.Where(x => x.IsAlive).Count();
@@ -20,35 +20,83 @@ namespace cli_life
             else
                 IsAliveNext = liveNeighbors == 3;
         }
+
         public void Advance()
         {
             IsAlive = IsAliveNext;
         }
     }
+
     public class Board
     {
         public readonly Cell[,] Cells;
         public readonly int CellSize;
 
-        public int Columns { get { return Cells.GetLength(0); } }
-        public int Rows { get { return Cells.GetLength(1); } }
-        public int Width { get { return Columns * CellSize; } }
-        public int Height { get { return Rows * CellSize; } }
+        public int Columns
+        {
+            get { return Cells.GetLength(0); }
+        }
 
-        public Board(int width, int height, int cellSize, double liveDensity = .1)
+        public int Rows
+        {
+            get { return Cells.GetLength(1); }
+        }
+
+        public int Width
+        {
+            get { return Columns * CellSize; }
+        }
+
+        public int Height
+        {
+            get { return Rows * CellSize; }
+        }
+
+        public Board(string filePath, int width, int height, int cellSize, double liveDensity)
         {
             CellSize = cellSize;
-
-            Cells = new Cell[width / cellSize, height / cellSize];
+            Cells = new Cell[width / CellSize, height / CellSize];
             for (int x = 0; x < Columns; x++)
-                for (int y = 0; y < Rows; y++)
-                    Cells[x, y] = new Cell();
+            for (int y = 0; y < Rows; y++)
+                Cells[x, y] = new Cell();
 
             ConnectNeighbors();
-            Randomize(liveDensity);
+
+            if (string.IsNullOrEmpty(filePath))
+            {
+                Randomize(liveDensity);
+            }
+            else
+            {
+                LoadFromFile(filePath);
+            }
+        }
+
+        private void LoadFromFile(string filePath)
+        {
+            filePath = Path.Combine(Directory.GetCurrentDirectory(), filePath);
+            try
+            {
+                using StreamReader reader = new StreamReader(filePath);
+                for (int row = 0; row < Cells.GetLength(1); row++)
+                {
+                    var line = reader.ReadLine();
+                    var split = line.ToCharArray();
+                    for (int col = 0; col < Cells.GetLength(0); col++)
+                    {
+                        var cell = Cells[col, row];
+                        cell.IsAlive = split[col].Equals('*');
+                    }
+                }
+            }
+            catch (FileNotFoundException e)
+            {
+                throw new FileLoadException($"Не удалось прочитать файл {filePath}");
+            }
         }
 
         readonly Random rand = new Random();
+
         public void Randomize(double liveDensity)
         {
             foreach (var cell in Cells)
@@ -62,6 +110,7 @@ namespace cli_life
             foreach (var cell in Cells)
                 cell.Advance();
         }
+
         private void ConnectNeighbors()
         {
             for (int x = 0; x < Columns; x++)
@@ -86,22 +135,30 @@ namespace cli_life
             }
         }
     }
+
     class Program
     {
-        static Board board;
-        static private void Reset()
+        private static Board board;
+        private static Startup _startup;
+
+        private static void Reset()
         {
+            _startup = new Startup();
+            var config = _startup._appConfig;
+
             board = new Board(
-                width: 50,
-                height: 20,
-                cellSize: 1,
-                liveDensity: 0.5);
+                filePath: config.LoadFilePath,
+                width: config.BoardHeight,
+                height: config.BoardWidth,
+                cellSize: config.BoardCellSize,
+                liveDensity: config.BoardLiveDensity);
         }
-        static void Render()
+
+        private static void Render()
         {
             for (int row = 0; row < board.Rows; row++)
             {
-                for (int col = 0; col < board.Columns; col++)   
+                for (int col = 0; col < board.Columns; col++)
                 {
                     var cell = board.Cells[col, row];
                     if (cell.IsAlive)
@@ -113,18 +170,56 @@ namespace cli_life
                         Console.Write(' ');
                     }
                 }
+
                 Console.Write('\n');
             }
         }
-        static void Main(string[] args)
+
+        private static void Main(string[] args)
         {
             Reset();
-            while(true)
+            int iterCount = 0;
+
+            while (true)
             {
                 Console.Clear();
+                Console.WriteLine($"Итерация: {++iterCount}");
                 Render();
                 board.Advance();
-                Thread.Sleep(1000);
+
+                if (iterCount % 5 == 0)
+                {
+                    CreateFile();
+                }
+
+                Thread.Sleep(400);
+            }
+        }
+
+        private static void CreateFile()
+        {
+            string filePath = Path.Combine(Directory.GetCurrentDirectory(), "..\\..\\..\\last_state.txt");
+
+            using (StreamWriter streamWriter = File.CreateText(filePath))
+            {
+                var cells = board.Cells;
+                for (var x = 0; x < cells.GetLength(0); x++)
+                {
+                    for (var y = 0; y < cells.GetLength(1); y++)
+                    {
+                        var cell = cells[x, y];
+                        if (cell.IsAlive)
+                        {
+                            streamWriter.Write('*');
+                        }
+                        else
+                        {
+                            streamWriter.Write(' ');
+                        }
+                    }
+
+                    streamWriter.WriteLine();
+                }
             }
         }
     }
